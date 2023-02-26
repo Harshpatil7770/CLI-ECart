2d33e62bcd0c68c14bae078842fc158abc00e8ea
Harsh <harshawardhanpatil7770@gmail.com>
2023-02-26 16:04:55 +0530

    Added Eureka-Server

---
+++ CLI-Eureka-Server/src/test/java/com/cli/ecart/CliEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cli.ecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
